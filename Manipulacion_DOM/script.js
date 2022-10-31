a1469d01714908aee0e156bd739894b9f1bd266b
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-10-30 21:39:18 -0500

    3C | Logging html elements

---
+++ Manipulacion_DOM/script.js
@@ -0,0 +1,13 @@
+let h1 = document.querySelector('h1');
+let p = document.querySelector('p');
+let parrafito = document.querySelector('.parrafito');
+let pid = document.querySelector('#pid');
+let input = document.querySelector('input');
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input
+});
